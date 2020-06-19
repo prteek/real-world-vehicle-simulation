446d154d1721dee84e982c0df28d321274528cbf
Prateek <prteek@users.noreply.github.com>
2020-06-19 03:52:54 +0100

    now importing all the variables of simulation by their own name

--- post_processing_simulation_result.py
+++ post_processing_simulation_result.py
@@ -2,13 +2,13 @@ import plotly.graph_objects as go
 import numpy as np
 from utility_functions import *
 
-import vehicle_simulation_model as vs # This will run the simulation
+from vehicle_simulation_model import * # This will run the simulation and import all the variables generated in the process
 
-x = vs.time
-fig = plt_plot(x=x, y=vs.speed_meters_per_second*3.6, name='speed [km/h]')
-fig.add_scatter(x=x,y=vs.battery_soc*100, name='battery soc [%]')
-fig.add_scatter(x=x,y=vs.battery_current/10, name='battery current [A]')
-fig.add_scatter(x=x,y=(np.append(0, np.diff(vs.timestamp))>1800)*np.append(0, np.diff(vs.timestamp))/60,  name='charging window [minutes]')
+x = time
+fig = plt_plot(x=x, y=speed_meters_per_second*3.6, name='speed [km/h]')
+fig.add_scatter(x=x,y=battery_soc*100, name='battery soc [%]')
+fig.add_scatter(x=x,y=battery_current/10, name='battery current [A]')
+fig.add_scatter(x=x,y=(np.append(0, np.diff(timestamp))>1800)*np.append(0, np.diff(timestamp))/60,  name='charging window [minutes]')
 fig.update_layout(title='Example day of driving',
                   xaxis_title='time [sec]',
                   xaxis_range=[0, 3830],
