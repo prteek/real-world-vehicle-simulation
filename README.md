462a9035b4b584691ba92ee07491fca2de4b05b9
prateek <prateek.patel@bricklane.com>
2023-04-19 08:15:42 +0100

    using simulation output in readme

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # real-world-vehicle-simulation
-Modelling for real world behaviour study
+Modelling to understand the behaviour of a real world vehicle
+
+This repo is an example in how would an electric vehicle behave if it were to replace a conventional vehicle  
+using a sample driving trip.  
+The idea is to extract behaviours relevant to battery namely 
+* Battery current
+* Battery State of charge
+* Effect of opportunistic charging
+
+
+![sim_result](./simulation_output.png)
--- post_processing_simulation_result.py
+++ post_processing_simulation_result.py
@@ -6,18 +6,19 @@ import matplotlib.pyplot as plt
 
 from vehicle_simulation_model import *  # This will run the simulation and import all the variables generated in the process
 
-x = time
-plt.plot(x, speed_meters_per_second * 3.6, label="speed [km/h]")
-plt.plot(x, battery_soc * 100, label="battery soc [%]")
-plt.plot(x, battery_current / 10, label="battery current [A]")
-plt.plot(x, (np.append(0, np.diff(timestamp)) > 1800) * np.append(0, np.diff(timestamp)) / 60,
+fig, ax = plt.subplots(figsize=(10, 5))
+ax.plot(time, speed_meters_per_second * 3.6, label="speed [km/h]")
+ax.plot(time, battery_soc * 100, label="battery soc [%]")
+ax.plot(time, battery_current / 10, label="battery current [A]")
+ax.plot(time, (np.append(0, np.diff(timestamp)) > 1800) * np.append(0, np.diff(timestamp)) / 60,
     label="charging window [minutes]",
 )
-plt.title("Example day of driving")
-plt.xlabel("time [sec]")
-plt.ylabel("signal values")
-plt.xlim([0, 3830])
-plt.ylim([0, 150])
-plt.legend()
+ax.set_title("Example day of driving")
+ax.set_xlabel("time [sec]")
+ax.set_ylabel("signal values")
+ax.set_xlim([0, 3830])
+ax.set_ylim([0, 150])
+ax.legend()
+ax.grid()
 plt.show()
-plt.savefig("simulation_output.png")
+fig.savefig("simulation_output.png")
